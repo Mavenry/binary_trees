7635e6fbe39897936d1389792a9501ec20127d9b
Mavenry <sundaybukola10@gmail.com>
2024-06-06 09:56:18 -0700

    added solution

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,7 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
         return (1);
 
     if (tree->left != NULL && tree->right != NULL)
-        return (is_full_binary_tree(tree->left) && is_full_binary_tree(tree->right));
+        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
     return (0);
 }
