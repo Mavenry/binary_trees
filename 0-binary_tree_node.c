9dfa64e8219f7a0dbed6dcd18cee4c0ef2174da1
Mavenry <sundaybukola10@gmail.com>
2024-06-06 06:55:23 -0700

    updated task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,20 +9,19 @@
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *node;
+	binary_tree_t *nNode;
 
-	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	nNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
 	{
 		return (NULL);
 	}
 	
-        node->parent = parent;
-	node->n = value;
-	node->left = NULL;
-	node->right = NULL;
+        nNode->parent = parent;
+	nNode->n = value;
+	nNode->left = NULL;
+	nNode->right = NULL;
 	
-	return (node);
+	return (nNode);
 }
