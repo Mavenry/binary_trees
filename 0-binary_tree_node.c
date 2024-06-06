89f6b53699ebf963a3ea7c14e5937662bff8918b
Mavenry <sundaybukola10@gmail.com>
2024-06-06 06:25:36 -0700

    added solution to task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,12 +16,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-        else
-	{
-        	node->parent = parent;
-		node->n = value;
-		node->left = NULL;
-		node->right = NULL;
-	}
+	
+        node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	
 	return (node);
 }
